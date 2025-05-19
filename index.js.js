f6f6c6377073a1eb254953216ce8346b6c57ac59
Oreganonn <andreymaxmc@gmail.com>
2025-05-19 04:11:55 -0300

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,5 @@
+// Função que será chamada quando o botão for clicado
+function mostrar() {
+    // Altera o estilo do parágrafo para exibi-lo
+    document.getElementById("mostrar").style.display = "block";
+}
